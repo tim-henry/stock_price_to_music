a3d7120575a9dd01dab13c252ec8706ffc7531e3
Timothy Henry <timhenry@mit.edu>
2017-12-23 23:21:42 -0500

    Cleanup

--- midi_creator.py
+++ midi_creator.py
@@ -6,20 +6,20 @@ filename = "test_music/twitter_prices.mid"
 
 start = datetime.datetime(2016,3,10)
 end = datetime.date.today()
-# stock = "YAMHF" #Yamaha
-stock = "TWTR" #Twitter
+
+twitter_abbreviation = "TWTR" #Twitter
 
 desired_high = 120
 desired_low = 10
 
-apple = web.DataReader(stock, "yahoo", start, end)
-prices = apple.get("Open")
+twitter_stock = web.DataReader(twitter_abbreviation, "yahoo", start, end)
+prices = twitter_stock.get("Open")
 
 low = min(prices)
 high = max(prices)
 print(low)
 print(high)
-print(apple.get("Open"))
+print(prices)
 
 
 desired_range = desired_high - desired_low
@@ -47,46 +47,5 @@ MyMIDI.addTempo(track, time, tempo)
 for i, pitch in enumerate(new_prices):
     MyMIDI.addNote(track, channel, int(pitch), time + i*duration, duration, volume)
 
-# for i in range(12):
-#     while time < 12:
-#         MyMIDI.addNote(track, channel, pitch, time*.25 + i*3, duration, volume)
-#         time += 1
-#         pitch += 6
-#     time = 0
-#     pitch -= (6*12 - 1)
-
-# count = 12
-# num = 0
-# track    = 0
-# channel  = 0
-# time     = 0    # In beats
-# duration = 1./8    # In beats
-# tempo    = 360   # In BPM
-# volume   = 60  # 0-127, as per the MIDI standard
-#
-# MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
-#                       # automatically)
-# MyMIDI.addTempo(track, time, tempo)
-
-# for i, pitch in enumerate(new_prices):
-#     MyMIDI.addNote(track, channel, int(pitch), time + i*duration, duration, volume)
-
-# for i in range(600):
-#     MyMIDI.addNote(track, channel, random.randint(10, 110), time + i * duration, duration, volume)
-# pitch = 110
-# while count > 0:
-#     for i in range(pitch,40,-4):
-#         MyMIDI.addNote(track, channel, i, num * duration, duration, volume)
-#         num += 1
-#     pitch -= 10
-#     count -= 1
-# pitch = 110
-# while count > 0:
-#     for i in range(6):
-#         print(((pitch - (12 * i)) + count))
-#         MyMIDI.addNote(track, channel, ((pitch - (6 * i)) + count), num * duration, duration, volume)
-#         num += 1
-#     pitch -= 1
-#     count -= 1
 with open(filename, "wb") as output_file:
     MyMIDI.writeFile(output_file)
